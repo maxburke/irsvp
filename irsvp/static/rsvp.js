edf03810cd508a0af6273dccb2d385c109079c52
Max Burke <maxburke@gmail.com>
2011-12-03 11:15:51 -0800

    Support pressing enter in the code field on the rsvp page. Also added a drop-down to indicate number of respondants.

--- irsvp/static/rsvp.js
+++ irsvp/static/rsvp.js
@@ -6,28 +6,47 @@ function invalidRsvp(xhr, errorString, exception) {
 
 function confirmedRsvp(json) {
     $('#rsvp-email').val(json.email);
-    $('#rsvp-number').val(json.numGuests);
+    var selectionDropdown = 'I <select id="rsvp-number-box">'
+        + '<option value="0">will not be coming :(</option>'
+        + '<option value="1">will be coming!</option>';
+    for (var i = 1; i < json.numGuests; ++i) {
+        selectionDropdown += '<option value="' 
+            + (i + 1) 
+            + '">will be coming with ' 
+            + i + ' guest' 
+            + (i > 1 ? 's' : '')
+            + '</option>';
+    }
+    selectionDropdown += '</select>';
+    $('#rsvp-number').html(selectionDropdown);
     $('#rsvp-special').val(json.special);
     $('#rsvp-details').addClass('validated');
     $('#rsvp-confirm').addClass('hidden');
 }
 
+function handleSubmit() {
+    var urlEndIndex = document.URL.length - 1;
+    var documentUrl = document.URL;
+    if (documentUrl[urlEndIndex] != "/") {
+        documentUrl = documentUrl + "/";
+    }
+    documentUrl = documentUrl + $('#rsvp-code').val();
+    var ajaxRequest = {
+        url : documentUrl,
+        type : "GET",
+        dataType : "json",
+        error : invalidRsvp,
+        success : confirmedRsvp
+    };
+    $.ajax(ajaxRequest)
+}
+
 function init() {
-    $('#rsvp-submit').click(function() {
-        var urlEndIndex = document.URL.length - 1;
-        var documentUrl = document.URL;
-        if (documentUrl[urlEndIndex] != "/") {
-            documentUrl = documentUrl + "/";
+    $('#rsvp-code').keypress(function(event) {
+        if (event.which === 13) {
+            handleSubmit();
         }
-        documentUrl = documentUrl + $('#rsvp-code').val();
-        var ajaxRequest = {
-            url : documentUrl,
-            type : "GET",
-            dataType : "json",
-            error : invalidRsvp,
-            success : confirmedRsvp
-        };
-        $.ajax(ajaxRequest)
     });
+    $('#rsvp-submit').click(handleSubmit);
 }
 
